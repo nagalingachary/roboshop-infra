1d204a2857bab739cb73a2f3a09a75b1a2e47dc4
nagalingachary <sivanagalingachary@gmail.com>
2023-09-16 20:13:19 +0530

    terraform user

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+module "this" {
+    source = "../terraform-aws-vpc"
+    cidr_block = var.cidr_block
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    igw_tags = var.internet_gateway_tags
+
+    # Public Subnet creation
+    public_subnet_cidr = var.public_subnet_cidr
+    azs = var.azs
+    public_subnet_names = var.public_subnet_names
+
+    # Private Subnet creation
+    private_subnet_cidr = var.private_subnet_cidr
+    private_subnet_names = var.private_subnet_names
+
+    # Database Subnet creation
+    database_subnet_cidr = var.database_subnet_cidr
+    database_subnet_names = var.database_subnet_names
+
+    public_route_table_tags = var.public_route_table_tags
+    private_route_table_tags = var.private_route_table_tags
+    database_route_table_tags = var.database_route_table_tags
+  }
